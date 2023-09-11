6d1f404396c4a19e213ea47549531652ae1480f0
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-11 12:52:35 +0500

    continue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import Todo from './com/Todo';
+
 
 function App() {
-  const [count ,setcount]=useState(0)
-  useEffect(()=>{
-console.log("empty array");
-  },[])
-  useEffect(()=>{
-console.log("no value");
-  },)
-  useEffect(()=>{
-    console.log("array plus passing variable");
-      },[count])
+//   const [count ,setcount]=useState(0)
+//   useEffect(()=>{
+// console.log("empty array");
+//   },[])
+//   useEffect(()=>{
+// console.log("no value");
+//   },)
+//   useEffect(()=>{
+//     console.log("array plus passing variable");
+//       },[count])
   return (
     <div className="App">
-<input type="text" value={count} />
-<button onClick={()=>setcount(count+1)}>Add</button>
+     <Todo/>
+{/* <input type="text" value={count} /> */}
+{/* <button onClick={()=>setcount(count+1)}>Add</button> */}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/com/Todo.js
+++ src/com/Todo.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './Todo.css'
 
 export default function Todo() {
   return (
-    <div>Todo</div>
+    <div className='center'>
+      <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Add Todo" aria-label="Username" required aria-describedby="basic-addon1"/>
+  <button className='btn btn-success' onClick={}>Add</button>
+</div>
+
+    </div>
   )
 }
