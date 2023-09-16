fb23eb53c4e3fee38ff0393a28fcac58b8e8b846
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-16 19:55:58 +0500

    complete

--- src/com/ErrorHandling.js
+++ src/com/ErrorHandling.js
@@ -1,104 +1,100 @@
-import React ,{useState,useEffect}from 'react'
-import './todolist.css'
-import './Todo.css'
+import React, { useEffect, useState } from 'react';
+import './todolist.css';
+import './Todo.css';
 import Swal from 'sweetalert2';
-const data1 =()=>{
-    // localStorage.getItem("list")
-    if(data1){
-    
-        return JSON.parse( localStorage.getItem("list"))
-    }
-    else{
-        return []
-    }
-}
-export default function ErrorHandling() {
 
-    const [input ,setInput]= useState("");
-    const [item ,setItem]= useState(data1());
-    const [btn ,setBtn]= useState(true)
-    const [select ,setSelect]= useState(null)
-    const Submit =()=>{
-        if(!input){
-            Swal.fire({
-                icon: 'error',
-                title: 'Please Fill This Input',
-                
-              })
+const getDataFromLocalStorage = () => {
+  const storedData = localStorage.getItem("list");
+  if (storedData) {
+    return JSON.parse(storedData);
+  } else {
+    return [];
+  }
+}
+
+export default function Faiz() {
+  const [input, setInput] = useState("");
+  const [item, setItem] = useState(getDataFromLocalStorage()); // Use the function to get data
+  const [btn, setBtn] = useState(true);
+  const [select, setSelect] = useState(null);
+
+  console.log(item);
+
+  const Submit = () => {
+    if (!input) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Please Fill This Input',
+      });
+    } else if (input && select) {
+      setItem(item.map((el) => {
+        if (el.id === select) {
+          return { ...el, name: input };
         }
-        else if(input && select){
-    setItem(
-        item.map((el)=>{
-            console.log(el.id);
-            console.log(select);
-            if(el.id==select){
-                return{...el,name:input}
-            }
-            return el
-        })
-    )
-    setBtn(true)
-    setInput("")
-    setSelect(null)
-        }
-        else{
-    let obj = {id:new Date().getTime().toString(),name:input}
-    console.log(obj);
-            setItem([...item,obj])
-            setInput("")
-            // console.log(item);
-            // console.log(input);
-        }
-        
+        return el;
+      }));
+      setBtn(true);
+      setInput("");
+      setSelect(null);
+    } else {
+      const obj = { id: new Date().getTime().toString(), name: input };
+      setItem([...item, obj]);
+      setInput("");
     }
-    const Del = (e)=> {
-        console.log(e);
-        let save = item.filter((ele)=>{
-    return e!==ele.id
-        });
-        setItem(save)
-    }
-    const Edit=(e)=>{
-        let store = item.find((ele)=>{
-            return e===ele.id
-            })
-            setBtn(false)
-            setInput(store.name)
-            setSelect(e)
-    }
-    useEffect(()=>{
-        let data = localStorage.setItem("list",JSON.stringify((item)))
-        },[item])
+  }
+
+  const Del = (e) => {
+    const save = item.filter((ele) => e !== ele.id);
+    setItem(save);
+  }
+
+  const Edit = (e) => {
+    const store = item.find((ele) => e === ele.id);
+    setBtn(false);
+    setInput(store.name);
+    setSelect(e);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(item));
+  }, [item]);
+
   return (
     <>
-    <h1 className=''>Todo List 📝</h1>
-<div className='center'>
-  {/* <form action="" > */}
-
-  <div class="input-group mb-3">
-<input type="text" class="form-control" placeholder="Add Todo" aria-label="Username" value={input} required aria-describedby="basic-addon1" onChange={(e)=>setInput(e.target.value)} />
-{
-  
-  btn? <button className='btn btn-success' type='submit' onClick={Submit}>Add</button>: <button className='btn btn-success' type='submit' onClick={Submit}>Edit</button>
-}
-</div>
-
-{
-    item.map((element)=>{
-        // console.log(e.id);
-        // let hi = e
-return(
-<ul className='row' key={element.id}>
-<h6 className=''>{element.name} <button className='btn btn-sm hi btn-danger' onClick={()=>Del(element.id)}><i class="fa-solid fa-trash"></i></button><button className='btn btn-success btn-sm ' onClick={()=>Edit(element.id)}><i class="fa-solid fa-pen-to-square"></i></button></h6>
-  
-    <hr/>
-</ul>
-)
-    })
-}
- 
-
-</div>
-</>
-  )
+      <h1 className=''>Todo List 📝</h1>
+      <div className='center'>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add Todo"
+            aria-label="Username"
+            value={input}
+            required
+            aria-describedby="basic-addon1"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          {btn ? (
+            <button className='btn btn-success' type='submit' onClick={Submit}>Add</button>
+          ) : (
+            <button className='btn btn-success' type='submit' onClick={Submit}>Edit</button>
+          )}
+        </div>
+        {item.map((element) => (
+          <ul className='row' key={element.id}>
+            <h6 className=''>
+              {element.name}
+              <button className='btn btn-sm hi btn-danger' onClick={() => Del(element.id)}>
+                <i className="fa-solid fa-trash"></i>
+              </button>
+              <button className='btn btn-success btn-sm ' onClick={() => Edit(element.id)}>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </button>
+            </h6>
+            <hr />
+          </ul>
+        ))}
+      </div>
+    </>
+  );
 }
